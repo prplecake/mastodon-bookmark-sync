2a232c176ad9d75cef15514f65f466b4f46d8270
prplecake <me@prplecake.com>
2022-11-01 21:08:35 -0500

    Don't allow posts with strict visibility settings to be saved to a bookmarking service

--- bookmarks.go
+++ bookmarks.go
@@ -19,14 +19,25 @@ type account struct {
 }
 
 func processBookmarks(data []byte) ([]bookmark, error) {
-	var bookmarks []bookmark
-	if err := json.Unmarshal(data, &bookmarks); err != nil {
+	var rawBookmarks []bookmark
+	if err := json.Unmarshal(data, &rawBookmarks); err != nil {
 		return nil, err
 	}
 	if debug {
 		var jsonResult bytes.Buffer
 		_ = json.Indent(&jsonResult, data, "", "  ")
 		log.Print(jsonResult.String())
+	}
+
+	var bookmarks []bookmark
+	for _, bookmark := range rawBookmarks {
+		if bookmark.Visibility == "private" || bookmark.Visibility == "direct" {
+			if debug {
+				log.Printf("Removing bookmark %s due to visibility setting", bookmark.URL)
+			}
+		} else {
+			bookmarks = append(bookmarks, bookmark)
+		}
 	}
 
 	return bookmarks, nil
