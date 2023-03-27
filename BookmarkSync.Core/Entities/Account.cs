bd7299eedd26f9d9b1d9de5f7cbba1f1538648bf
Matthew Jorgensen <me@prplecake.com>
2023-03-27 02:33:54 -0500

    Update Account, add constructor

--- BookmarkSync.Core/Entities/Account.cs
+++ BookmarkSync.Core/Entities/Account.cs
@@ -3,6 +3,11 @@ namespace BookmarkSync.Core.Entities;
 public class Account
 {
     public string? Name { get; set; }
+    public Account(string name)
+    {
+        Name = name;
+    }
+    public Account() { }
     /// <inheritdoc />
     public override string ToString() => Name ?? GetType().Name;
 }
