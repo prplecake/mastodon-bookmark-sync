acbb73cbab89ac5f5d96a1a030c24c109e6d723e
Matthew Jorgensen <me@prplecake.com>
2023-03-27 04:05:45 -0500

    Add HtmlUtilities to sanitize HTML

--- BookmarkSync.Core/Entities/Bookmark.cs
+++ BookmarkSync.Core/Entities/Bookmark.cs
@@ -1,9 +1,16 @@
+using BookmarkSync.Core.Utilities;
+
 namespace BookmarkSync.Core.Entities;
 
 public class Bookmark
 {
     public Account? Account { get; set; }
-    public string? Content { get; set; }
+    private string? _content;
+    public string? Content
+    {
+        get => _content != null ? HtmlUtilities.ConvertToPlainText(_content) : "";
+        set => _content = value;
+    }
     public string? Id { get; set; }
     public string? Uri { get; set; }
     public string? Visibility { get; set; }
