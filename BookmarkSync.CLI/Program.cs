dab70108ad2de6e8039e1c346bdfa343ce1dccc6
Matthew Jorgensen <me@prplecake.com>
2023-03-27 01:33:31 -0500

    Refactor Program.cs
    - Implement Serilog
    - Use HostedService

--- BookmarkSync.CLI/Program.cs
+++ BookmarkSync.CLI/Program.cs
@@ -1,42 +1,54 @@
 ﻿using System;
-using System.Threading.Tasks;
 using BookmarkSync.Core.Configuration;
-using BookmarkSync.Core.Interfaces;
 using BookmarkSync.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace BookmarkSync.CLI;
 
 public class Program
 {
     private static IConfiguration? _configuration;
-    public async static Task Main(string[] args)
+    public static int Main(string[] args)
     {
         _configuration = SetupConfiguration(args);
         IConfigManager configManager = new ConfigManager(_configuration);
+        
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(_configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
 
-        using var host = Host.CreateDefaultBuilder(args)
+        try
+        {
+            Log.Information("Starting host");
+            BuildHost(args, configManager).Run();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+            return 1;
+        }
+        finally
+        {
+            // Shutdown
+            configManager.SaveToFile();
+            Log.CloseAndFlush();
+        }
+    }
+    public static IHost BuildHost(string[] args, IConfigManager configManager) =>
+        new HostBuilder()
             .ConfigureServices(services =>
             {
                 services.AddSingleton(configManager);
 
-                services.AddTransient<IBookmarkingService, PinboardBookmarkingService>();
+                services.AddHostedService<BookmarkSyncService>();
             })
+            .UseSerilog()
             .Build();
-        await host.StartAsync();
-        var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
-
-        // Do work here
-        Console.WriteLine("Hello, world!");
-
-        // Shutdown
-        configManager.SaveToFile();
-
-        lifetime.StopApplication();
-        await host.WaitForShutdownAsync();
-    }
     private static IConfiguration SetupConfiguration(string[] args) => new ConfigurationBuilder()
         .AddJsonFile("appsettings.json", false, true)
         .AddEnvironmentVariables()
