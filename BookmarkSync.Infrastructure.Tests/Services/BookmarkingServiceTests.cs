fe8ca0a62604d59fad0428fb1efe010ea56ab849
Matthew Jorgensen <me@prplecake.com>
2023-03-28 23:56:24 -0500

    Cleanup file

--- BookmarkSync.Infrastructure.Tests/Services/BookmarkingServiceTests.cs
+++ BookmarkSync.Infrastructure.Tests/Services/BookmarkingServiceTests.cs
@@ -9,6 +9,28 @@ namespace BookmarkSync.Infrastructure.Tests.Services;
 public class BookmarkingServiceTests
 {
     [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void GetBookmarkingService_Exception()
+    {
+        // Arrange
+        var config = new Dictionary<string, string?>
+        {
+            {
+                "App:Bookmarking:Service", "Nonsense"
+            }
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(config)
+            .Build();
+
+        IConfigManager configManager = new ConfigManager(configuration);
+
+        // Act
+        var unused = BookmarkingService.GetBookmarkingService(configManager);
+
+        // Assert - Exception
+    }
+    [TestMethod]
     public void GetBookmarkingService_Pinboard()
     {
         // Arrange
@@ -33,27 +55,5 @@ public class BookmarkingServiceTests
         // Assert
         Assert.AreEqual(obj.GetType(), typeof(PinboardBookmarkingService));
         Assert.IsInstanceOfType(obj, typeof(PinboardBookmarkingService));
-    }
-    [TestMethod]
-    [ExpectedException(typeof(InvalidOperationException))]
-    public void GetBookmarkingService_Exception()
-    {
-        // Arrange
-        var config = new Dictionary<string, string?>
-        {
-            {
-                "App:Bookmarking:Service", "Nonsense"
-            }
-        };
-        var configuration = new ConfigurationBuilder()
-            .AddInMemoryCollection(config)
-            .Build();
-
-        IConfigManager configManager = new ConfigManager(configuration);
-
-        // Act
-        var unused = BookmarkingService.GetBookmarkingService(configManager);
-
-        // Assert - Exception
     }
 }
