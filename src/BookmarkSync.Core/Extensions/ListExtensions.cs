406ff785984d184f79183f999285d8625614cc6a
Matty Jorgensen <me@prplecake.com>
2025-03-14 11:12:20 -0500

    Fix NullReferenceException

--- src/BookmarkSync.Core/Extensions/ListExtensions.cs
+++ src/BookmarkSync.Core/Extensions/ListExtensions.cs
@@ -6,10 +6,11 @@ public static class ListExtensions
 {
     public static List<Bookmark>? RemoveAllFromIgnoredAccounts(
         this List<Bookmark>? bookmarks,
-        List<string> ignoredAccounts)
+        List<string>? ignoredAccounts)
     {
         if (bookmarks is null) return bookmarks;
-        bookmarks.RemoveAll(b => ignoredAccounts.Contains(b.Account.Name));
+        if (ignoredAccounts is null) return bookmarks;
+        bookmarks.RemoveAll(b => ignoredAccounts.Contains(b.Account!.Name!));
         return bookmarks;
     }
 }
